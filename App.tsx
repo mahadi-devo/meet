abf5702307e802baf47405082a53dd0781f2eca0
Mahadi Bro <mahadibro@Mahadis-MacBook-Air.local>
2023-03-01 21:54:51 +0530

    basic ui impl

--- App.tsx
+++ App.tsx
@@ -5,23 +5,51 @@
  * @format
  */
 
-import React, {useState, useEffect, useCallback} from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import React, { useState, useEffect, useCallback } from 'react';
+import { Text, View } from 'react-native';
 // @ts-ignore
-import JitsiMeet, {JitsiMeetView} from 'react-native-jitsi-meet';
+import JitsiMeet, { JitsiMeetView } from 'react-native-jitsi-meet';
 
 function App(): JSX.Element {
   const [showJitsiView, setShowJitsiView] = useState(false);
 
   useEffect(() => {
     setTimeout(() => {
-      const url = 'EquivalentUpdatesBenefitFar';
+      const url = 'https://meet.jit.si/InterimShakesFallMightily';
       const userInfo = {
         displayName: 'User',
         email: 'user@example.com',
         avatar: 'https:/gravatar.com/avatar/abc123',
       };
-      JitsiMeet.call(url, userInfo);
+      const options = {
+        audioMuted: false,
+        audioOnly: false,
+        videoMuted: false,
+        subject: 'TEST CALL',
+      };
+      // const meetFeatureFlags = {
+      //   addPeopleEnabled: false,
+      //   calendarEnabled: false,
+      //   callIntegrationEnabled: true,
+      //   chatEnabled: false,
+      //   closeCaptionsEnabled: false,
+      //   inviteEnabled: false,
+      //   androidScreenSharingEnabled: true,
+      //   liveStreamingEnabled: false,
+      //   meetingNameEnabled: false,
+      //   meetingPasswordEnabled: false,
+      //   pipEnabled: false,
+      //   kickOutEnabled: false,
+      //   conferenceTimerEnabled: true,
+      //   videoShareButtonEnabled: false,
+      //   recordingEnabled: false,
+      //   reactionsEnabled: false,
+      //   raiseHandEnabled: false,
+      //   tileViewEnabled: false,
+      //   toolboxEnabled: true,
+      //   welcomePageEnabled: false,
+      // };
+      JitsiMeet.call(url, userInfo, options);
       console.log('Called');
       setShowJitsiView(true);
     }, 1000);
@@ -37,6 +65,7 @@ function App(): JSX.Element {
     console.log('Meet UI Init');
     return (
       <>
+        {/* To Avoid black screen in android */}
         {showJitsiView && <Text>{Math.random().toString()}</Text>}
         <View
           style={{
@@ -47,7 +76,7 @@ function App(): JSX.Element {
             onConferenceTerminated={() => JitsiMeet.endCall()}
             onConferenceJoined={() => console.log('joined')}
             onConferenceWillJoin={() => console.log('will join')}
-            style={{flex: 1, height: '100%', width: '100%'}}
+            style={{ flex: 1, height: '100%', width: '100%' }}
           />
         </View>
       </>
